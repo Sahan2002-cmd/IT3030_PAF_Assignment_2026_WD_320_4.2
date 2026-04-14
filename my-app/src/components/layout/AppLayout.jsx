32928460f19895eab4e05983a347de1ddfb3f468
Sahan2002-cmd <it23837676@.my.sliit.lk>
2026-04-14 20:02:53 +0530

    implement app layout and slide bar . Using react file and Css file for this task

---
+++ my-app/src/components/layout/AppLayout.jsx
@@ -0,0 +1,16 @@
+// src/components/layout/AppLayout.jsx
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import './AppLayout.css';
+
+export default function AppLayout() {
+  return (
+    <div className="app-layout">
+      <Sidebar />
+      <main className="app-main">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
